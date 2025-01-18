c393366ee43ff464307e71c2c45459ea72df50b2
elnai534 <elinachin513@gmail.com>
2025-01-18 16:04:26 -0700

    Lay out changes for Locations.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,8 +3,9 @@
 export default function Layout({ children }) {
     return (
         
+        
         <div className="min-h-screen flex flex-col">
-
+            <Toolbar />
             
             <main className="flex-grow">{children}</main>
 
