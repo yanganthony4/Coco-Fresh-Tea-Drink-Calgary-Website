92f6fff3f717f5e53355ada6a5860050e6504176
elnai534 <elinachin513@gmail.com>
2025-01-21 18:43:30 -0700

    update cetegorylists

--- app/components/CategoryList.js
+++ app/components/CategoryList.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const CategoryList = ({ categories, onSelectCategory, selectedCategory }) => {
   return (
-    <div className="w-1/4 p-6 bg-gray-100 h-screen">
+    <div className="w-1/4 pl-8 border-r border-gray-300 h-screen">
       <ul className="space-y-4 text-lg font-semibold">
         {categories.map((category, index) => (
           <li
             key={index}
             onClick={() => onSelectCategory(category)}
             className={`cursor-pointer hover:text-orange-600 ${
-              selectedCategory === category ? 'text-orange-600' : 'text-gray-800'
+              selectedCategory === category ? 'text-orange-500 font-bold' : 'text-gray-800'
             }`}
           >
             {category}
