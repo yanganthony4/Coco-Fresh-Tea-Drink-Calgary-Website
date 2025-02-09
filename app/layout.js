14c116983552af3f5a341f3c03141faeb216a526
elnai534 <elinachin513@gmail.com>
2025-02-09 11:18:11 -0700

    Microsoft Clarity installation in layout.js

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
               />
           </head>
           <body className="bg-white">
+              {/*  Google Tag Manager (gtag.js) */}
               <Script
                   strategy="afterInteractive"
                   src="https://www.googletagmanager.com/gtag/js?id=G-3S12T44MMW"
@@ -29,12 +30,26 @@ export default function RootLayout({ children }) {
                           window.dataLayer = window.dataLayer || [];
                           function gtag(){dataLayer.push(arguments);}
                           gtag('js', new Date());
-                           gtag('config', 'G-3S12T44MMW');
+                          gtag('config', 'G-3S12T44MMW');
                       `,
                   }}
               />
-              
-              
+
+              {/*  Microsoft Clarity Tracking Code */}
+              <Script
+                  id="microsoft-clarity"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                      __html: `
+                          (function(c,l,a,r,i,t,y){
+                              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                          })(window, document, "clarity", "script", "q3ynfe4cyj");
+                      `,
+                  }}
+              />
+
               <Toolbar />
               <main>{children}</main>
           </body>
