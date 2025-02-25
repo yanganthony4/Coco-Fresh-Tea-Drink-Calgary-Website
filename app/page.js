6d88f44740c7d8e926410429db2e0dbead107aa2
yanganthony4 <yanganthony4@gmail.com>
2025-02-25 14:24:07 -0700

    Renamed Boxes.js to see if this would fix the Vercel error
    Renamed Boxes.js to see if this would fix the Vercel error

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link"
 import DrinkImageSlider from "./components/DrinkImageSlider"
-import Boxes from "./components/Boxes"
+import Boxes from "./components/HomePageInformationalBoxes"
 import DeliveryAppCarousel from "./components/DeliveryAppCarousel"
 import ImageSlider from "./components/ImageSlider"
 
