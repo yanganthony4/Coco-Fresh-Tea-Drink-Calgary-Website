c14271cff3827daf28fe5850af933283800c1e1c
elnai534 <elinachin513@gmail.com>
2025-04-08 14:35:08 -0400

    Update Delivery App carousel to fix 404 issue with image

--- app/components/DeliveryAppCarousel.tsx
+++ app/components/DeliveryAppCarousel.tsx
@@ -37,7 +37,7 @@ const DeliveryAppCarouselMap = () => {
   }, [smallImages.length]);
 
   const handleImageError = (index: number) => {
-    const fallbackSrc = "/images/deliverydriver.webp";
+    const fallbackSrc = "/images/main-image.webp";
     if (index === -1) {
       setMainImage((prev) => ({ ...prev, src: fallbackSrc }));
     } else {
