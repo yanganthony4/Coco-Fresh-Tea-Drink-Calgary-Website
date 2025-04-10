b2687773723c2a8eb50cdc82afc30c832f9e0847
yanganthony4 <yanganthony4@gmail.com>
2025-04-09 20:24:38 -0600

    Revert "Merge branch 'drink-builder-ui' into about-us-page"
    This reverts commit 7d3783040803eb89bfa474be2c5abf1cf7c10a29, reversing
    changes made to bfc34e79b798f87e912b3f2c7c8660e81c2ac651.

--- app/components/textarea.js
+++
@@ -1,8 +0,0 @@
-"use client"
-
-export function Textarea({ className = "", ...props }) {
-  return (
-    <textarea className={`w-full p-2 border rounded-md bg-white text-gray-900 resize-none ${className}`} {...props} />
-  )
-}
-
